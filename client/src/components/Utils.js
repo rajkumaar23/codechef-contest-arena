33a410b6d25c9cdb2e9a2b1b323444b47d2af84b
Rajkumar S <rajkumaar2304@icloud.com>
2020-03-24 21:56:56 +0530

    Fixes URL issue
    Signed-off-by: Rajkumar S <rajkumaar2304@icloud.com>

--- client/src/components/Utils.js
+++ client/src/components/Utils.js
@@ -5,7 +5,7 @@
 export default class Utils {
     static ACCESS_TOKEN = 'access_token';
     static REFRESH_TOKEN = 'refresh_token';
-    static API_URL = 'http://0.0.0.0:2304';
+    static API_URL = (window.location.hostname === 'localhost' ? 'http://0.0.0.0:2304' : 'https://codechef-arena.herokuapp.com');
     static BACKEND_URL = Utils.API_URL;
 
     static isLoggedIn() {
